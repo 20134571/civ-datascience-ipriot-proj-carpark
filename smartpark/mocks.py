61996fa0afad9af7620f1c7cd29c66313a99c2a9
Adam Bradley <20134571@tafe.wa.edu.au>
2025-11-19 16:15:26 +0800

    logging OK, Counter OK, Duration included

--- smartpark/mocks.py
+++ smartpark/mocks.py
@@ -32,16 +32,16 @@ class MockCarparkManager(CarparkSensorListener,CarparkDataProvider):
 
     def __init__(self):
         self.configuration = parse_config(MockCarparkManager.CONFIG_FILE, "Queen Street")
-        #configuration = parse_config(MockCarparkManager.CONFIG_FILE,"Queen Street")
+        
         self._temperature = 30
         self._total_spaces = self.configuration ["total_spaces"]
         self.unuseable_spaces = self.configuration.get("unuseable_spaces",0)
         self._available_spaces = self._total_spaces - self.unuseable_spaces
-        #Self._available_spaces = 999
-        #set log_file file path
+        
         log_filename = self.configuration.get ("log_file", "")
         self.log_file = os.path.join(self.LOGGING, log_filename)
-        self.cars_log = [{'license_plate': '1DKH682', 'time_in': '17:44:41', 'time_out': None, 'status': 'In', 'date_in': '2025-11-18', 'date_out': None}]
+        #self.cars_log = [{'license_plate': '1DKH682', 'time_in': '17:44:41', 'time_out': None, 'status': 'In', 'date_in': '2025-11-18', 'date_out': None}]
+        self.cars_log = []
         self._total_cars_in = 0 #len(self.cars_log)
 
         # configure logging once
@@ -79,33 +79,24 @@ class MockCarparkManager(CarparkSensorListener,CarparkDataProvider):
         car.enter()
         event_date=  time.strftime("%Y-%m-%d")
         event_time= time.strftime("%H:%M:%S")
-        #license = Car(license_plate)
-        #new_car_record = {"license_plate": license_plate, "time_in": event_time, "status": "In", "date_in" : event_date}
+        
         new_car_record = car.car_info()
         self.cars_log.append(new_car_record)
         self.log_record(car)  # write to log
         print(f"Entry {new_car_record['license_plate']} logged at {new_car_record['time_in']}")
-        #if self._update_event:
-        #    self._update_event.set()
         
-
     def outgoing_car(self,license_plate):
         license_plate = license_plate.upper()
         
         event_time = time.strftime("%H:%M:%S")
         event_date=  time.strftime("%Y-%m-%d")
-        #license = Car(license_plate)
-
+        
         car_found = None
-        #time_in = None
-        #date_in = None  
-
+        
         for record in self.cars_log:
             if record["license_plate"] == license_plate and record["status"] == "In":
                 car_found = record
-                #time_in = record.get("time_in",None)
-                #date_in = record.get("date_in",None)
-                #print(self.cars_log)
+                
                 break
 
         if car_found is not None:
@@ -114,24 +105,40 @@ class MockCarparkManager(CarparkSensorListener,CarparkDataProvider):
             
             car.time_in = car_found["time_in"]
             car.date_in = car_found["date_in"]
+                     
+            # Define date/time formats used in Car.enter() and Car.exit()
+            DATE_FORMAT = "%Y-%m-%d"
+            TIME_FORMAT = "%H:%M:%S"
+            
+            try:
+                # Combine date and time strings into full datetime objects
+                datetime_in_str = f"{car.date_in} {car.time_in}"
+                datetime_out_str = f"{car.date_out} {car.time_out}"
 
-            exit_car_record = {"license_plate": license_plate, "time_in": car.time_in, "time_out" : event_time, "status": "Out", "date_in" : car.date_in, "date_out" : event_date}
-            self.cars_log.remove(car_found)  #if new licence plate is in list, Car.car_info(self.license_plate), then remove from list
+                datetime_in = datetime.strptime(datetime_in_str, f"{DATE_FORMAT} {TIME_FORMAT}")
+                datetime_out = datetime.strptime(datetime_out_str, f"{DATE_FORMAT} {TIME_FORMAT}")
+
+                # Calculate timedelta
+                duration_td: timedelta = datetime_out - datetime_in
+                
+                # Convert duration to total minutes (rounded to nearest minute) and store it
+                # The duration is stored in the Car object for logging
+                car.duration_minutes = round(duration_td.total_seconds() / 60)
+                
+                print(f"DEBUG: Duration calculated: {car.duration_minutes} minutes")
+                
+            except Exception as e:
+                print(f"Error calculating duration for {license_plate}: {e}")
+           
+           #exit_car_record = {"license_plate": license_plate, "time_in": car.time_in, "time_out" : event_time, "status": "Out", "date_in" : car.date_in, "date_out" : event_date}
+            self.cars_log.remove(car_found)  # remove from list
             self._total_cars_in -= 1
 
             final_log_data = car.car_info()
             print(f"DEBUG: Logging exit data -> {final_log_data}")
 
-            #self.cars_log.append(exit_car_record) - this is wrong as it writes to the variable cars_log not the text log. 
             self.log_record(car)  # write to log
-            #exit_log = {
-            #    "license": license_plate,
-            #    "time_in": time_in, 
-            #    "time_out": event_time, 
-            #    "event": "Out", 
-            #    "date in" : date_in, 
-            #    "date_out" : event_date
-            #}             
+
 
         else:
             car = Car(license_plate)
@@ -139,19 +146,10 @@ class MockCarparkManager(CarparkSensorListener,CarparkDataProvider):
             
             car.time_in = "N/A" 
             car.date_in = "N/A"
-
-            #car.time_in = "None"
-            #car.time_out = event_time, 
-            #car.status = "Out"
-            #car.date_in = "None", 
-            #car.date_out = event_date
-            #self.cars_log.append({"license_plate": license, "time_in": "None", "time_out" : event_time, "status": "Out", "date_in" : "None", "date_out" : event_date})
+           
             self.log_record(car) # write to log
-        
+                     
         print('Car out! ' + license_plate)
-        #if self._update_event:
-        #    self._update_event.set()
-        #self.log_record(Car(license_plate))  # write to log
 
     #logging method
     def log_record(self,car):   
@@ -203,6 +201,7 @@ class Car:
         self.time_out = None
         self.date_in = None
         self.date_out = None
+        self.duration_minutes = None    
         self.status = "Out" # if it is not in it is out and In is iniated by the enter process
 
     def enter(self):
